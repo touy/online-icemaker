6eccf32b055e5bd4af7ef41210e226d903cac4be
KouiDeveloper <kouiprogramer@gmail.com>
2018-05-29 17:10:15 +0700

    update

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,20 +1,150 @@
-import { Component, Inject, OnInit, OnDestroy } from "@angular/core";
+import { Component, Inject, OnInit, OnDestroy, Input ,IterableDiffers, DoCheck } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 import { Router, RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms"; // <<<< import it here
 import { WebsocketDataServiceService } from "../websocket-data-service.service";
 import { ChatService, Message } from "../chat.service";
 import { WebsocketService } from "../websocket.service";
+import { BOOL_TYPE } from "@angular/compiler/src/output/output_ast";
+import { BADFAMILY } from "dns";
 
+import { ElementRef,ViewChild} from '@angular/core';
 @Component({
   selector: "app-list-product",
   templateUrl: "./list-product.component.html",
   styleUrls: ["./list-product.component.css"],
   providers: [WebsocketDataServiceService, ChatService, WebsocketService]
 })
-export class ListProductComponent implements OnInit, OnDestroy {
+  export class ListProductComponent implements DoCheck {
+
+    ngDoCheck() {
+      var changes = this.differ.diff(this.productionCollection);
+      if (changes) {
+        console.log(changes);
+        console.log('line chart get new device'+this.productionCollection.length);
+        this.bindLineChart();
+      }
+    }
+  
+    @Input() productionCollection:any[];
+    @Input() differ: any;
+    @Input() _selectedMonth=new Date().getMonth()+1;
+    @Input() _selectedYear=new Date().getFullYear();
+    
+    public lineChartData: Array<any> = [
+      { data: [10, 20, 16, 18, 15, 16, 17, 18, 16, 20], label: "Working hours" },
+      { data: [15, 24, 15, 15, 15, 15, 15, 16, 16, 16], label: "Resting hours" },
+      { data: [12, 10, 1, 0, 0, 16, 5, 4, 4, 4, 4, 4, 4], label: "Problem hours" }
+    ];
+   
+    private month = 1;
+    private day = 1;
+    private year=1;
+    sortProduction(){
+      this.productionCollection.sort(function(a, b){return a.day - b.day});
+    }
+  
+    bindLineChart(){
+      this.sortProduction();
+      this.lineChartData=[];
+      let w=[];
+      let p=[];
+      let pr=[];
+      for (let index = 0; index < this.productionCollection.length; index++) {
+        const element = this.productionCollection[index];
+        if(element.productiontime){
+          w.push(element.productiontime.working);
+          p.push(element.productiontime.parking);
+          pr.push(element.productiontime.problem);
+        }
+       // console.log([w,p,pr]);
+      }
+      //this.lineChartData.push([w,p,pr]);
+      this.lineChartData=[
+        { data: w, label: "Working hours" },
+        { data: p, label: "Resting hours" },
+        { data: pr, label: "Problem hours" }
+      ];
+    }
+    daysInMonth(month, year): any[] {
+      let d = new Date(year, month, 0).getDate();
+      let arr = [];
+      for (let index = 0; index < d; index++) {
+        arr.push(index + 1);
+      }
+      return arr;
+    }
+    setLInChartLabels(m, y) {
+      this.lineChartLabels = this.daysInMonth(m, y);
+    }
+    public lineChartLabels: Array<any> = this.daysInMonth(
+      new Date().getMonth() + 1,
+      new Date().getFullYear()
+    );
+    public lineChartOptions: any = {
+      responsive: true
+    };
+    public lineChartColors: Array<any> = [
+      {
+        // green
+        backgroundColor: "rgba(140, 247, 0, 0.253)",
+        borderColor: "rgba(148,159,177,1)",
+        pointBackgroundColor: "rgba(148,159,177,1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(148,159,177,0.8)"
+      },
+      {
+        // blue
+        backgroundColor: "rgba(1, 48, 255, 0.63)",
+        borderColor: "rgba(77,83,96,1)",
+        pointBackgroundColor: "rgba(77,83,96,1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(77,83,96,1)"
+      },
+      {
+        // red
+        backgroundColor: "rgba(255, 0, 0, 0.2)",
+        borderColor: "rgba(148,159,177,1)",
+        pointBackgroundColor: "rgba(148,159,177,1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(148,159,177,0.8)"
+      }
+    ];
+    public lineChartLegend: boolean = true;
+    public lineChartType: string = "line";
+  
+    public randomize(): void {
+      let _lineChartData: Array<any> = new Array(this.lineChartData.length);
+      for (let i = 0; i < this.lineChartData.length; i++) {
+        _lineChartData[i] = {
+          data: new Array(this.lineChartData[i].data.length),
+          label: this.lineChartData[i].label
+        };
+        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+          _lineChartData[i].data[j] = Math.floor(Math.random() * 100 + 1);
+        }
+      }
+      this.lineChartData = _lineChartData;
+    }
+  
+    // events
+    public chartClicked(e: any): void {
+      console.log(e);
+    }
+  
+    public chartHovered(e: any): void {
+      console.log(e);
+    }
+  
+    closeResult: string;
+    @ViewChild('Alert_update_details') Alert_update_details: ElementRef;
+    
   private _message: Message;
-  private _newUser: any = {};
-  private _userDetailsStr = "";
+  private _deviceInfo: any [] ;
+  private _selectedDevice:any;
   private _server_event: Array<any> = [];
   private _client: Message = {
     gui: "",
@@ -30,8 +160,9 @@ export class ListProductComponent implements OnInit, OnDestroy {
   private _otherMessage: any = {};
   private _subs: any = [];
   private _trans: any = [];
-
+  
   //// ICE-MAKER
+  
   private _currentDevice: any;
   private _arrayDevices: any;
   private _currentPayment: any;
@@ -39,62 +170,43 @@ export class ListProductComponent implements OnInit, OnDestroy {
   private _currentBill: any;
   private _arrayBills: any;
   private _arrayPayment: any;
+  
+  
+  
   /// WEBSOCKET LAUNCHING
   constructor(
+    private modalService: NgbModal,
     private websocketDataServiceService: WebsocketDataServiceService,
-    private router: Router
+    private router: Router,
+    private differs: IterableDiffers
   ) {
+    this.differ = differs.find([]).create(null);
     this.loadClient();
+    // if (this._client.logintoken) {
+    //     router.navigate(["/main-menu"]);
+    //   }
     this._subs.push(
       this.websocketDataServiceService.clientSource.subscribe(client => {
         this.readClient(client);
       })
     );
-    this._subs.push(
-      this.websocketDataServiceService.newUserSource.subscribe(client => {
-        this._newUser = client;
-        this.readNewUser(client);
-      })
-    );
-    // this._subs.push(this.websocketDataServiceService.eventSource.subscribe(events => {
-    //   this._server_event = events;
-    //   this.readServerEvent(events);
-    // }));
-    this._subs.push(
-      this.websocketDataServiceService.currentUserSource.subscribe(user => {
-        this.readCurrentUserDetail(user);
-      })
-    );
-
-    this._subs.push(
-      this.websocketDataServiceService.otherSource.subscribe(msg => {
-        this.readOtherMessage(msg);
-      })
-    );
-    this._subs.push(
-      this.websocketDataServiceService.currentBillSource.subscribe(msg => {
-        this.readBill(msg);
-      })
-    );
-    this._subs.push(
-      this.websocketDataServiceService.currentDeviceSource.subscribe(msg => {
-        this.readDevice(msg);
-      })
-    );
-    this._subs.push(
-      this.websocketDataServiceService.currentPaymentSource.subscribe(msg => {
-        this.readPayment(msg);
-      })
-    );
-    this._subs.push(
-      this.websocketDataServiceService.currentSubUserSource.subscribe(msg => {
-        this.readSubUser(msg);
-      })
-    );
-
+  
+   this._currentDevice=[];
+  console.log('load line chart');
   }
   //// END WEBSOCKET LAUNCHING
-
+  
+  
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
+  }
+  Show_update_details(Alert_update_details,d){
+    this._selectedDevice=d;
+    this.modalService.open(Alert_update_details,{ centered: true }); 
+    // alert(content);   
+  } 
+  
+  
   /// OTHER FUNCTIONS
   private clearJSONValue(u) {
     for (const key in u) {
@@ -103,19 +215,21 @@ export class ListProductComponent implements OnInit, OnDestroy {
       }
     }
   }
-
+  
   //// END OTHER FUNCTIONS
   /// INIT FUNCTIONS
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
-    this._newUser = JSON.parse(JSON.stringify(this._client));
-    this._newUser.data = {};
-    this._newUser.data.user = {};
-    this._message = JSON.parse(JSON.stringify(this._client));
-    this._currentUserdetail = {};
-
-    this._userDetailsStr = "";
+    this._currentSubUser=[];
+    this._message = JSON.parse(JSON.stringify(this._client));;
     this._otherMessage = {};
+    
+    this.runInit();
+  }
+  runInit(){
+    setTimeout(() => {
+      //this.loadDevices();
+    }, 1000);
   }
   ngOnDestroy() {
     console.log("STOP SERVICE");
@@ -135,217 +249,48 @@ export class ListProductComponent implements OnInit, OnDestroy {
     // }
   }
   /// INIT FUNCTIONS
-
+  
   /// *************RECEIVING  */
-  ab2str(arrayBuffer) {
-    let binaryString = "";
-    const bytes = new Uint8Array(arrayBuffer),
-      length = bytes.length;
-    for (let i = 0; i < length; i++) {
-      binaryString += String.fromCharCode(bytes[i]);
-    }
-    return binaryString;
-  }
-
-  str2ab(str) {
-    const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    const bufView = new Uint8Array(buf);
-    for (let i = 0, strLen = str.length; i < strLen; i++) {
-      bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-  }
-  readClient(c): any {
-    // this._client
-    try {
-      if (c !== undefined) {
-        this._client = c;
-        // this.saveClient();
-        console.log(c);
-        switch (this._client.data["command"]) {
-          case "heart-beat":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              // this._client.data['user'] = u;
-              console.log("heart beat ok");
-            }
-            break;
-          case "ping":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              console.log(this._client.data["message"]);
-            }
-            break;
-          case "get-client":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              console.log("get-client OK");
-            }
-            break;
-          case "shake-hands":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              // // console.log(this._client);
-              console.log(this._client.data["message"]);
-            } else {
-              console.log("shake hands ok");
-              // this.getDevices();
-              // alert("get device don't work any more");
-            }
-            break;
-          case "get-transaction":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              // // alert('change password OK');
-              console.log("get transaction id ok");
-            }
-            break;
-          case "check-transaction":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              // // alert('change password OK');
-              console.log("check transaction id ok");
-            }
-            break;
-          case "get-all-paymnet":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              alert("Get-all-paymnet is working");
-            }
-            break;
-          case "get-device-info":
-            if (
-              this._client.data["message"].toLowerCase().indexOf("error") > -1
-            ) {
-              console.log(this._client.data["message"]);
-            } else {
-              alert("Get-device-info is working");
-            }
-            break;
-          default:
-            break;
+  
+    readClient(c): any {
+      // this._client
+      try {
+        if (c !== undefined) {
+          this._client = c;
+          // this.saveClient();
+         // console.log(c);
+          switch (this._client.data["command"]) {
+            case "ping":
+              if (
+                this._client.data["message"].toLowerCase().indexOf("error") > -1
+              ) {
+                console.log(this._client.data["message"]);
+              } else {
+                console.log(this._client.data["message"]);
+              }
+              break;
+              
+             
+            default:
+              break;
+  
+          }
+        } else {
+          // alert('data empty');
+          console.log("data is empty");
         }
-        // console.log(this.heartbeat_interval);
-        // console.log(this._client);
-        // if (evt.data != '.') $('#output').append('<p>'+evt.data+'</p>');
-      } else {
-        // alert('data empty');
-        console.log("data is empty");
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
+   
     }
-  }
-  readNewUser(n): any {
-    // this._newUser;
-    if (n !== undefined) {
-      this._newUser.data = n.data;
-    }
-  }
-  readCurrentUserDetail(c: any): any {
-    // this._currentUserDetail
-    if (c !== undefined) {
-      this._currentUserdetail = c;
-    }
-  }
-  readOtherMessage(m: any): any {
-    // this._message
-    if (m !== undefined) {
-      this._message = m;
-    }
-  }
-  readBill(m: any) {
-    if (m !== undefined) {
-      if (Array.isArray(m)) {
-        this._currentBill = m;
-      } else {
-        this._arrayBills = m;
-      }
-    }
-  }
-  readSubUser(m: any) {
-    if (m !== undefined) {
-      this._currentSubUser = m;
-    }
-  }
-  readDevice(m: any) {
-    if (m !== undefined) {
-      if (Array.isArray(m)) {
-        this._arrayDevices = m;
-      } else {
-        this._currentDevice = m;
-      }
-    }
-  }
-  readPayment(m: any) {
-    if (m !== undefined) {
-      if (Array.isArray(m)) {
-        this._currentPayment = m;
-      } else {
-        this._arrayPayment = m;
-      }
-    }
-  }
-
+  
+  
   /// END RECEIVING
-
+  
   //// SENDING
-  showNewMessage() {
-    this._client.data.message = "changed from show message";
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this.websocketDataServiceService.changeMessage(this._client);
-  }
-  setOtherMessage() {
-    const msg = {
-      title: "",
-      data: {},
-      other: {} // ...
-    };
-    // msg.data['transaction'] = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.setOtherMessage(msg);
-  }
-  shakeHands() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this.websocketDataServiceService.shakeHands();
-    this.saveClient();
-  }
-  ping_test() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this._client.data.message += " HERE in app component";
-    // console.log(this._client);
-    this.websocketDataServiceService.ping_test();
-  }
+  
   /////////////// END SENDING
   /// ICEMAKER ----------------------------------------
-
-  getDeviceInfo() {
-    const d = this._currentDevice;
-    this.websocketDataServiceService.getDeviceInfo(d);
   }
-
-  getAllPayment() {
-    this.websocketDataServiceService.getAllPayment();
-  }
-}
+  
